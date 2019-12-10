ac47b4e2d6cbdb5d763d17e2cf007ea0a2ebe797
Louis Capitanchik <contact@louiscap.co>
2019-12-10 10:57:39 +0000

    Update value definition for random types

--- src/datatypes.rs
+++ src/datatypes.rs
@@ -1,5 +1,9 @@
 use uuid;
 use serde_derive::{Deserialize, Serialize};
+use fake::{Faker, Fake};
+use fake::faker;
+use rand::Rng;
+use std::ops::Add;
 
 /// An Enum that represents all of the possible random data generation types.
 /// Where the type is a struct, it should be represented as a nested map type, where the outer
@@ -117,6 +121,38 @@ impl std::string::ToString for RandomData {
     }
 }
 
+/// Create a number of a certain length. The value is returned as a string for display,
+/// and can therefore represent a number of arbitrary length at the expense of higher memory
+/// consumption.
+///
+/// The first digit will be in the range 1-9, whilst the following digits will be between 0-9.
+///
+/// # Examples
+///
+/// ```rust
+/// let three_digit_number = number_with_length(3);
+/// println!("{}", three_digit_number);
+/// ```
+fn number_with_length(length: usize) -> String {
+    let mut random = rand::thread_rng();
+    let mut buffer = String::with_capacity(length);
+    buffer = buffer + &format!("{}", random.gen_range(1, 10));
+
+    for _ in 0..length - 1 {
+        buffer = buffer + &format!("{}", random.gen_range(0, 10));
+    }
+
+    buffer
+}
+
+#[test]
+fn generate_number_format_of_correct_length() {
+    assert_eq!(number_with_length(1).len(), 1);
+    assert_eq!(number_with_length(2).len(), 2);
+    assert_eq!(number_with_length(10).len(), 10);
+    assert_eq!(number_with_length(1000).len(), 1000);
+}
+
 /// Use a `RandomData` definition to generate a random string of data
 ///
 /// # Examples
@@ -131,24 +167,29 @@ impl std::string::ToString for RandomData {
 /// ```
 pub fn generate_fake_data(spec: RandomData) -> String {
     match spec {
-        RandomData::FirstName => format!("{}", fake!(Name.first_name)),
-        RandomData::LastName => format!("{}", fake!(Name.last_name)),
-        RandomData::FullName => format!("{}", fake!(Name.name)),
-        RandomData::Email => format!("{}", fake!(Internet.safe_email)),
-        RandomData::Number { digits } => format!("{}", fake!(Number.number(digits))),
-        RandomData::NumberBetween { min, max } => format!("{}", fake!(Number.between(min, max))),
-        RandomData::Company => format!("{}", fake!(Company.name)),
-        RandomData::City => format!("{}", fake!(Address.city)),
-        RandomData::StreetAddress => format!("{}", fake!(Address.street_address)),
-        RandomData::Latitude => format!("{}", fake!(Address.latitude)),
-        RandomData::Longitude => format!("{}", fake!(Address.longitude)),
-        RandomData::LatLong => format!(r#"[{}, {}]"#, fake!(Address.latitude), fake!(Address.longitude)),
-        RandomData::LongLat => format!(r#"[{}, {}]"#, fake!(Address.longitude), fake!(Address.latitude)),
-        RandomData::GeoPoint => format!(r#"POINT({} {})"#, fake!(Address.longitude), fake!(Address.latitude)),
-        RandomData::Postcode => format!("{}", fake!(Address.postcode)),
-        RandomData::FullAddress => format!("{}, {}, {}", fake!(Address.street_address), fake!(Address.city), fake!(Address.postcode)),
+        RandomData::FirstName => format!("{}", faker::name::en::FirstName().fake::<String>()),
+        RandomData::LastName => format!("{}", faker::name::en::LastName().fake::<String>()),
+        RandomData::FullName => format!("{}", faker::name::en::Name().fake::<String>()),
+        RandomData::Email => format!("{}", faker::internet::en::SafeEmail().fake::<String>()),
+        RandomData::Number { digits } => format!("{}", number_with_length(digits)),
+        RandomData::NumberBetween { min, max } => format!("{}", rand::thread_rng().gen_range(min, max)),
+        RandomData::Company => format!("{}", faker::company::en::CompanyName().fake::<String>()),
+        RandomData::City => format!("{}", faker::address::en::CityName().fake::<String>()),
+        RandomData::StreetAddress => format!("{}", faker::address::en::StreetName().fake::<String>()),
+        RandomData::Latitude => format!("{}", faker::address::en::Latitude().fake::<String>()),
+        RandomData::Longitude => format!("{}", faker::address::en::Longitude().fake::<String>()),
+        RandomData::LatLong => format!(r#"[{}, {}]"#, faker::address::en::Latitude().fake::<String>(), faker::address::en::Longitude().fake::<String>()),
+        RandomData::LongLat => format!(r#"[{}, {}]"#, faker::address::en::Longitude().fake::<String>(), faker::address::en::Latitude().fake::<String>()),
+        RandomData::GeoPoint => format!(r#"POINT({} {})"#, faker::address::en::Longitude().fake::<String>(), faker::address::en::Latitude().fake::<String>()),
+        RandomData::Postcode => format!("{}", faker::address::en::PostCode().fake::<String>()),
+        RandomData::FullAddress => format!(
+            "{}, {}, {}",
+            faker::address::en::StreetName().fake::<String>(),
+            faker::address::en::CityName().fake::<String>(),
+            faker::address::en::PostCode().fake::<String>()
+        ),
         RandomData::UUID4 => format!("{}", uuid::Uuid::new_v4()),
-        RandomData::PhoneNumber => format!("{}", fake!(PhoneNumber.phone_number)),
+        RandomData::PhoneNumber => format!("{}", faker::phone_number::en::PhoneNumber().fake::<String>()),
         RandomData::LoremPicsum { width, height, grayscale } => format!(
             "https://picusm.photos/{}{}/{}",
             if grayscale.unwrap_or(false) { "g/" } else { "" },
