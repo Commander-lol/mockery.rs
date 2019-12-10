06d2001f9f0410c78be5f67e90ef159eb17cef37
Louis Capitanchik <contact@louiscap.co>
2019-12-10 11:53:18 +0000

    Update spec error handling

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,36 @@
 use mockery::{model, generation, cli, generator};
 use clap;
+use failure::{Error, Fail};
 
 fn main() {
     let args = cli::get_args_from_stdin();
-    process_args(args);
+    match process_args(args) {
+        Ok(_) => (),
+        Err(e) => eprintln!("{}", e),
+    }
 }
 
-fn process_args(args: cli::CliArgs) {
+/// Wrap a string in a failure compatible container until refactoring is complete
+#[derive(Debug, Fail)]
+enum StringErrorCompat {
+    #[fail(display = "{}", 0)]
+    S(String),
+}
+
+fn process_args(args: cli::CliArgs) -> Result<(), Error> {
 
     use mockery::specification::{Specification, self};
     extern crate serde_json;
 
     let model_name = &args.model_name;
-    let spec = specification::io::read_spec(&args.gen_spec_path).unwrap();
+    let spec = specification::io::read_spec(&args.gen_spec_path)?;
 
     if spec.has_model(&model_name) {
-        let data = generator::from_spec(model_name.clone(), spec.clone());
-
-        match data {
-            Ok(res) => {
-                generator::write_output(&args.output_path, res, spec, args.output_type);
-            },
-            Err(e) => println!("Error: {}", e),
-        }
+        let data = generator::from_spec(model_name.clone(), spec.clone()).map_err(|e| StringErrorCompat::S(e))?;
+        generator::write_output(&args.output_path, data, spec, args.output_type);
     } else {
         println!("No such model {} in {:?}", &model_name, &args.gen_spec_path.to_str());
     }
+
+    Ok(())
 }
--- src/specification.rs
+++ src/specification.rs
@@ -1,6 +1,7 @@
 use crate::datatypes::RandomData;
 use std::string::ToString;
 use std::iter::Iterator;
+use failure::Fail;
 
 use serde_derive::{Deserialize, Serialize};
 
@@ -53,18 +54,61 @@ impl Specification {
     }
 }
 
-
 pub mod io {
     use super::Specification;
+    use crate::generator::from_spec;
 
     use std::io::Result;
     use std::fs::read_to_string;
     use std::path::{Path, PathBuf};
     use std::convert::AsRef;
-    use serde_json::from_str;
+    use failure::{Fail};
 
-    pub fn read_spec<P: AsRef<Path>>(path: P) -> Option<Specification> {
-        let content = read_to_string(path).ok()?;
-        Some(from_str(&content).unwrap())
+    use serde_derive::{Deserialize, Serialize};
+    use serde_json::{from_str, error::Category};
+
+    pub type SpecResult<Success> = std::result::Result<Success, SpecError>;
+
+    #[derive(Debug, Fail)]
+    pub enum SpecError {
+        #[fail(display = "Could not find the spec file: {}", 0)]
+        MissingFile(String),
+        #[fail(display = "Failed for unhandled reason: {}", inner)]
+        IOError { inner: std::io::Error },
+        #[fail(display = "The provided spec path could not be correctly converted")]
+        BadPath,
+        #[fail(display = "The specification is not valid JSON")]
+        BadFormat,
+        #[fail(display = "The specification contained one or more invalid definitions")]
+        BadData
+    }
+
+    fn pathable_to_string<P: AsRef<Path>>(path: &P) -> SpecResult<String> {
+        return match path.as_ref().to_str() {
+            Some(string) => Ok(String::from(string)),
+            None => Err(SpecError::BadPath)
+        }
+    }
+
+    pub fn read_spec<P: AsRef<Path>>(path: P) -> SpecResult<Specification> {
+        let content = match read_to_string(&path) {
+            Ok(content) => content,
+            Err(e) => match e.kind() {
+                std::io::ErrorKind::NotFound => {
+                    return Err(SpecError::MissingFile(pathable_to_string(&path)?))
+                }
+                _ => return Err(SpecError::IOError { inner: e })
+            }
+        };
+
+        match from_str(&content) {
+            Ok(spec) => Ok(spec),
+            Err(e) => match e.classify() {
+                Category::Eof |
+                Category::Syntax => Err(SpecError::BadFormat),
+                Category::Data => Err(SpecError::BadData),
+                Category::Io => Err(SpecError::IOError { inner: e.into() }),
+            }
+        }
     }
 }
